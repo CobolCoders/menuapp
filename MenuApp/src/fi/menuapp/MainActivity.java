02c8fe9bb6b59819bb512801836047407abf808b
Jonne <a1202419@myy.haaga-helia.fi>
2014-10-16 15:05:44 +0300

    -Cart activity
    -Action bar menu buttons:
    	Menu -> Cart
    	Cart -> Menu

--- MenuApp/src/fi/menuapp/MainActivity.java
+++ MenuApp/src/fi/menuapp/MainActivity.java
@@ -1,10 +1,12 @@
 package fi.menuapp;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +21,21 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+    
+    @Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		if(id == R.id.toCart) {
+			Intent intent = new Intent(this, CartActivity.class);
+			startActivity(intent);
+		}
+		return super.onOptionsItemSelected(item);
+	}
     
 }
